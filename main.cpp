1611a77bb83948199127234b2104cf4ee0dad78f
thomas203 <desmots.thomas@gmail.com>
2018-12-11 18:44:13 +0100

    testt_price_call

--- main.cpp
+++ main.cpp
@@ -4,5 +4,7 @@
 int main(int argc, char* argv[])
 {
 	std::cout << "hello" << std::endl;
+	std::cout << dauphine::vanilla_payoff(120,100,true) << std::endl;
+	std::cout << dauphine::bs_price(100,100,0.2,5,true) << std::endl;
     return 0;
 }
