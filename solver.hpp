c7c60feb285c9ea9d61c3325736ade56d31a850b
thomas203 <desmots.thomas@gmail.com>
2018-12-11 22:46:06 +0100

    add cpp and hpp for solver

--- solver.hpp
+++ solver.hpp
@@ -5,13 +5,6 @@
 
 namespace dauphine
 {
-    double vanilla_payoff(double fwd, double strike, bool is_call);
-    double bs_time_value(double fwd, double strike, double volatility, double maturity);
-    double bs_price(double fwd, double strike, double volatility, double maturity, bool is_call);
-
-    std::vector<double> vanilla_payoff(const std::vector<double>& fwd, double strike, bool is_call);
-    std::vector<double> bs_time_value(const std::vector<double>& fwd, double strike, double volatility, double maturity);
-    std::vector<double> bs_price(const std::vector<double>& fwd, double strike, double volatility, double maturity, bool is_call);
 }
 
 #endif
