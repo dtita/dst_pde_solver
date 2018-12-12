cc32d4769bff43b85bf3480acde5c9de419547a5
thomas203 <desmots.thomas@gmail.com>
2018-12-12 11:20:25 +0100

    creation mesh

--- solver.cpp
+++ solver.cpp
@@ -5,6 +5,15 @@
 
 namespace dauphine
 {
-
+	mesh::mesh(double time_space, double space_space, double maturity, double spot_max)
+		: m_time_space(time_space), m_space_space(space_space), m_maturity(maturity),m_spot_max(spot_max)
+	{
+	}
+	mesh::~mesh()
+	{
+	}
+	double mesh::get_mesh_maturity() const{
+		return m_maturity;
+	}
 }
 
--- solver.hpp
+++ solver.hpp
@@ -5,6 +5,20 @@
 
 namespace dauphine
 {
+	class mesh {
+	public:
+		mesh(double time_space, double space_space,double maturity, double spot_max);
+		double get_mesh_time_space() const;
+		double get_mesh_maturity() const;
+		double get_mesh_space_space() const;
+		double get_mesh_spot_max() const;
+		~mesh();
+	private:
+		double m_time_space;
+		double m_space_space;
+		double m_maturity;
+		double m_spot_max;
+	};
 }
 
 #endif
