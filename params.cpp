f0923062eab7065fedf91624d1372f37e676075c
David Tiobang <dtiobang@gmail.com>
2018-12-14 19:00:59 +0100

    creation vecteur parametres

---
+++ params.cpp
@@ -0,0 +1,43 @@
+//
+//  params.cpp
+//  
+//
+//  Created by David Tiobang on 14/12/2018.
+//
+
+#include "params.hpp"
+#include "solver.hpp"
+#include <cmath>
+#include <limits>
+#include <algorithm>
+
+namespace dauphine
+{
+    
+    params::params(double dt, double dx, double maturity, double spot, std::vector<double> boundaries)
+    : m_dt(dt), m_dx(dx), m_maturity(maturity),m_spot(spot), m_spot_boundaries(boundaries)
+    {
+    }
+    
+    params::~params()
+    {
+    }
+    double params::get_maturity() const {
+        return m_maturity;
+    }
+    double params::get_dt() const {
+        return m_dt;
+    }
+    double params::get_dx() const {
+        return m_dx;
+    }
+    double params::get_spot() const {
+        return m_spot;
+    }
+    
+    std::vector<double> params::get_spot_boundaries() const {
+        return m_spot_boundaries;
+    }
+    
+    
+}
---
+++ params.hpp
@@ -0,0 +1,41 @@
+//
+//  params.hpp
+//  
+//
+//  Created by David Tiobang on 14/12/2018.
+//
+
+//#ifndef params_hpp
+//#define params_hpp
+#ifndef SOLVER_HPP
+#define SOLVER_HPP
+
+#include <vector>
+#include <stdio.h>
+
+namespace dauphine
+{
+    class params{
+        
+        public:
+            params(double dt, double dx,double maturity, double spot, std::vector<double> spot_boundaries);
+            double get_dt() const;
+            double get_maturity() const;
+            double get_dx() const;
+            double get_spot() const;
+            std::vector<double> spot_vector();
+            std::vector<double> get_spot_boundaries() const;
+            ~params();
+        
+        private:
+            double m_dt;
+            double m_dx;
+            double m_maturity;
+            double m_spot;
+            std::vector<double> m_spot_boundaries;
+        
+    };
+    
+}
+
+#endif /* params_hpp */
